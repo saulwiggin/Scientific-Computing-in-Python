9134192cf9b76cc1037d12df1baaa5ba521638bc
saul wiggin <saulwiggin@googlemail.com>
2014-05-19 08:04:50 +0000

    breakout02.py

---
+++ Breakout02.py
@@ -0,0 +1,52 @@
+
+# coding: utf-8
+
+# In[1]:
+
+get_ipython().run_cell_magic(u'file', u'age.py', u'')
+
+
+# In[8]:
+
+get_ipython().run_cell_magic(u'file', u'age.py', u'import datetime\nmybday = 1987-07-30 \ndate = datetime.datetime.now()\ntimedelta = date - mybday\nprint(timedelta)')
+
+
+# In[9]:
+
+run age.py
+
+
+# In[ ]:
+
+
+
+
+# In[1]:
+
+run age.py
+
+
+# In[3]:
+
+datatime.datetime()?
+
+
+# In[5]:
+
+print(datedate.datetime.now())
+
+
+# In[6]:
+
+print(datedate.datetime.now())
+
+
+# In[7]:
+
+print(datetime.datetime.now())
+
+
+# In[ ]:
+
+
+
